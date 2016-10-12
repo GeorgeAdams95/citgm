8c8d8cc01befdc8c364ea001ce3afdc8d4727579
George Adams <george.adams@uk.ibm.com>
2016-10-12 19:10:14 +0100

    Update test-reporter-append.js

--- test/reporter/test-reporter-append.js
+++ test/reporter/test-reporter-append.js
@@ -1,1 +1,10 @@
+'use strict';
+var test = require('tap').test;
 
+var reporter = require('../../lib/reporter');
+
+test('reporter.tap(tap, module, true):', function (t) {
+  t.ok(reporter.tap, 'you should be able to append');
+  t.equals(typeof reporter.markdown, 'function', 'tap is a function');
+  t.end();
+});
